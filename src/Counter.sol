918ad1304bf205b71b30e8756cdda48d310a5eba
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-10-26 12:07:51 -0500

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,22 +1,128 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
-// import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/forge-std/src/Test.sol";
 
 contract Counter {
     uint256 public number;
-
-    uint16 public number2;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
-    function setNumber2(uint16 newNumber) public {
-        number2 = newNumber;
-    }
-
     function increment() public {
         number++;
+    }
+}
+
+contract CounterTest is Test {
+    Counter counter;
+
+    function setUp() public {
+        
+        counter = new Counter();
+        
+    }
+
+    function test_Increment() public {
+        assertEq(counter.number(), 0);
+        counter.increment();
+        assertEq(counter.number(), 1);
+    }
+
+    function test_Increment_twice() public {
+        assertEq(counter.number(), 0);
+        counter.increment();
+        assertEq(counter.number(), 1);
+        counter.increment();
+        assertEq(counter.number(), 7);
+    }
+}
+
+contract ContractBTest is Test {
+    uint256 testNumber;
+
+    function setUp() public {
+        testNumber = 42;
+    }
+
+    function test_NumberIs42() public {
+        assertEq(testNumber, 42);
+    }
+
+    function test_CannotSubtract43() public {
+        vm.expectRevert(stdError.arithmeticError);
+        testNumber -= 43;
+    }    
+}
+
+error Unauthorized();
+
+contract OwnerUpOnly {
+    address public immutable owner;
+    uint256 public count;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function increment() external {
+        if (msg.sender != owner) {
+            revert Unauthorized();
+        }
+        count++;
+    }
+}
+
+contract OwnerUpOnlyTest is Test {
+    OwnerUpOnly upOnly;
+
+    function setUp() public {
+        upOnly = new OwnerUpOnly();
+    }
+
+    function test_IncrementAsOwner() public {
+        assertEq(upOnly.count(), 0);
+        upOnly.increment();
+        assertEq(upOnly.count(), 1);
+    }
+    function XtestFail_IncrementAsNotOwner() public {
+        //vm.expectRevert(Unauthorized.selector);
+        //vm.prank(address(0));
+        upOnly.increment();
+    }
+}
+
+
+contract EmitContractTest is Test {
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function Xtest_ExpectEmit() public {
+        ExpectEmit emitter = new ExpectEmit();
+        // Check that topic 1, topic 2, and data are the same as the following emitted event.
+        // Checking topic 3 here doesn't matter, because `Transfer` only has 2 indexed topics.
+        vm.expectEmit(true, true, false, true);
+        // The event we expect
+        emit Transfer(address(this), address(1337), 1337);
+        // The event we get
+        emitter.t();
+    }
+
+    function Xtest_ExpectEmit_DoNotCheckData() public {
+        ExpectEmit emitter = new ExpectEmit();
+        // Check topic 1 and topic 2, but do not check data
+        vm.expectEmit(true, true, false, false);
+        // The event we expect
+        emit Transfer(address(this), address(1337), 1338);
+        // The event we get
+        emitter.t();
+    }
+}
+
+contract ExpectEmit {
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function t() public {
+        emit Transfer(msg.sender, address(1337), 1337);
     }
 }
